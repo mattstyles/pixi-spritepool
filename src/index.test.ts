f3bdfca3ad88ad2587da0cef53c6522bf37cefd2
Matt Styles <matt@personalurban.com>
2022-01-01 12:05:57 +0000

    update build all script

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,6 @@
 import {SpritePool} from './index'
 
+// @TODO need to run these in a browser really
 test('Should allocate a list of sprites', () => {
   const length = 5
   const pool = SpritePool.of({
